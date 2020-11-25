7dad2be3efa98ff42598b3ab61c2ce5ec2c018b5
TheViperShow <29604693+TheViperShow@users.noreply.github.com>
2020-11-25 23:57:02 +0100

    Update 9.

--- src/main/java/me/thevipershow/bedwars/storage/sql/DataCleaner.java
+++ src/main/java/me/thevipershow/bedwars/storage/sql/DataCleaner.java
@@ -2,6 +2,7 @@ package me.thevipershow.bedwars.storage.sql;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import me.thevipershow.bedwars.LoggerUtils;
 import me.thevipershow.bedwars.storage.sql.tables.DailyQuestsTableCreator;
@@ -16,9 +17,10 @@ public final class DataCleaner {
         this.plugin = plugin;
     }
 
-    private static final String GET_EVENTSCHEDULER_STATUS = "select count(USER) from (select * from information_schema.PROCESSLIST where USER = 'event_scheduler') as res;";
-    private static final String START_DAILY_CLEAN_TASK = "CREATE EVENT IF NOT EXIST clean_daily ON SCHEDULE EVERY 1 DAY STARTS (TIMESTAMP(CURRENT_DATE) + INTERVAL 1 DAY) DO UPDATE " + DailyQuestsTableCreator.TABLE + " SET win_first=0,games_played=0;";
-    private static final String START_WEEKLY_CLEAN_TASK = "CREATE EVENT IF NOT EXIST clean_weekly ON SCHEDULE EVERY 7 DAY STARTS (TIMESTAMP(CURRENT_DATE) + INTERVAL 7 DAY) DO UPDATE " + WeeklyQuestsTableCreator.TABLE + " SET beds_broken=0;";
+    private static final String GET_EVENTSCHEDULER_STATUS = "select count(*) from (select * from information_schema.PROCESSLIST where USER = 'event_scheduler') as res where USER='event_scheduler';";
+    private static final String START_DAILY_CLEAN_TASK = "CREATE EVENT IF NOT EXISTS clean_daily ON SCHEDULE EVERY 1 DAY STARTS (TIMESTAMP(CURRENT_DATE) + INTERVAL 1 DAY) DO UPDATE " + DailyQuestsTableCreator.TABLE + " SET win_first=0,games_played=0;";
+    private static final String START_WEEKLY_CLEAN_TASK = "CREATE EVENT IF NOT EXISTS clean_weekly ON SCHEDULE EVERY 7 DAY STARTS (TIMESTAMP(CURRENT_DATE) + INTERVAL 7 DAY) DO UPDATE " + WeeklyQuestsTableCreator.TABLE + " SET beds_broken=0;";
+    private static final String ENABLE_SCHEDULER = "SET GLOBAL event_scheduler = ON;";
 
     private void inform() {
         LoggerUtils.logColor(plugin.getLogger(), "&eStarting MySQL data cleaner task.");
@@ -28,14 +30,17 @@ public final class DataCleaner {
         MySQLDatabase.getConnection().ifPresent(c -> {
 
             try (Connection connection = c;
-                 PreparedStatement statement = connection.prepareStatement(GET_EVENTSCHEDULER_STATUS);) {
-                int res = statement.executeUpdate();
-                if (res == 1) {
-                    try (PreparedStatement cleanDaily = connection.prepareStatement(START_DAILY_CLEAN_TASK);
-                         PreparedStatement cleanWeekly = connection.prepareStatement(START_WEEKLY_CLEAN_TASK)) {
-                        cleanDaily.executeUpdate();
-                        cleanWeekly.executeUpdate();
+                 PreparedStatement statement = connection.prepareStatement(GET_EVENTSCHEDULER_STATUS);
+                 ResultSet res = statement.executeQuery();) {
+                if (!res.next()) {
+                    try (PreparedStatement enableScheduler = connection.prepareStatement(ENABLE_SCHEDULER)) {
+                        enableScheduler.executeUpdate();
                     }
+                }
+                try (PreparedStatement cleanDaily = connection.prepareStatement(START_DAILY_CLEAN_TASK);
+                     PreparedStatement cleanWeekly = connection.prepareStatement(START_WEEKLY_CLEAN_TASK)) {
+                    cleanDaily.executeUpdate();
+                    cleanWeekly.executeUpdate();
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
